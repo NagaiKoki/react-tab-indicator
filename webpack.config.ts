c696c80b50d6530756c6b511692e408e22db360f
NagaiKoki <kokinagai10+1@gmail.com>
2020-12-31 14:29:36 +0900

    first commit

---
+++ webpack.config.ts
@@ -0,0 +1,29 @@
+import * as path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", "jsx"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: "/dist",
+    host: "0.0.0.0",
+  },
+};
+
+export default config;
